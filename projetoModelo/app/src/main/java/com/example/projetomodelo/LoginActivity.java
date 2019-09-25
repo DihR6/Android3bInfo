227c3e11eaa8da6c9f04409a6b69fd5a91792bda
Rubens Leme <rubenss.leme@gmail.com>
2019-09-24 21:03:30 -0300

    tela login

--- projetoModelo/app/src/main/java/com/example/projetomodelo/LoginActivity.java
+++ projetoModelo/app/src/main/java/com/example/projetomodelo/LoginActivity.java
@@ -2,13 +2,44 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+ public class LoginActivity extends AppCompatActivity {
+
+     Button btnEnviar;
+     Button btnCancelar;
+     EditText edtUsuario;
+     EditText edtSenha;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        inicializarObjeto();
+        abrirActivityMenu();
+    }
+
+    public void inicializarObjeto(){
+        btnEnviar = (Button) findViewById(R.id.btnEnviar);
+        btnCancelar =(Button) findViewById(R.id.btnCancelar);
+        edtSenha = (EditText) findViewById(R.id.edtSenha);
+        edtUsuario = (EditText) findViewById(R.id.edtUsuario);
+
+
+    }
+
+    private void abrirActivityMenu() {
+        btnEnviar.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View view) {
+                Intent intentActivityMenu = new Intent(getApplicationContext(),MenuActivity.class);
+                startActivity(intentActivityMenu);
+            }
+        });
     }
 }
